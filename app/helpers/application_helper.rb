2ea41698335e830c1ea5777dd551b63a8bb0498f
yucjk0120 <yu@hal.mu>
2015-05-06 14:55:33 +0900

    add granted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def is_granted(*responsibles)
+		if current_user.admin
+			return true
+		end
+
+		responsibles.class.each do |res|
+			if Responsible.where(:name => res.to_s, :user_id => current_user.id).count > 0
+				return true
+			end
+		end
+		return false
+	end
 end
