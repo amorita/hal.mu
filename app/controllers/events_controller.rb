0b6f64646712c74c803dacadbc030f09888673f4
yucjk0120 <yu@hal.mu>
2015-05-06 09:41:48 +0900

    remove layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
 
-#  before_filter :authenticate_user!
+  before_filter :authenticate_user!
+  layout false
   
   def index
   	@future_events = Event.where('`when` >= current_date()').order(:when)
