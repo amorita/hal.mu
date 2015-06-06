d3c6e8259a52fd9e62b077f7643fba39b43de034
yu <yu@hal.mu>
2015-06-06 14:26:32 +0900

    サブミット修正

--- app/controllers/camp_attendances_controller.rb
+++ app/controllers/camp_attendances_controller.rb
@@ -1,11 +1,15 @@
 class CampAttendancesController < ApplicationController
-
+layout 'priv'
 def edit
   @camp_att = CampAttendance.where(:user_id => current_user).first
   if @camp_att.nil?
     @camp_att = CampAttendance.new
     @camp_att.user_id = current_user.id
   end
+end
+
+def create
+  p hoge.hoge
 end
 
 end
