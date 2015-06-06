922005845c0e3aea338a3700874be05dc18191ab
yucjk0120 <yu@hal.mu>
2015-06-06 14:41:49 +0900

    camp contoller create

--- app/controllers/camp_attendances_controller.rb
+++ app/controllers/camp_attendances_controller.rb
@@ -8,8 +8,26 @@ def edit
   end
 end
 
+def show
+  @camp_att = CampAttendance.where(:user_id => current_user).first
+end
+
 def create
-  p hoge.hoge
+  @camp_att = CampAttendance.where(:user_id => current_user).first
+  if @camp_att.nil?
+    @camp_att = CampAttendance.new
+    @camp_att.user_id = current_user.id
+  end
+  @camp_att.by_car = params[:by_car]
+  @camp_att.memo = params[:memo]
+  params[:selections].each_char.with_index(1) do |val, idx|
+    @camp_att.send(('section_' + idx).to_sym) = val
+  end
+  if @camp_att.save!
+    redirct_to @camp_att
+  else
+    render 'edit'
+  end
 end
 
 end
