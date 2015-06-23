3868fde559804f03ba3a61dcfcf86ed78e65485c
yucjk0120 <yu@hal.mu>
2015-06-24 01:56:48 +0900

    add cutoff

---
+++ app/models/fee_cutoffs.rb
@@ -0,0 +1,2 @@
+class FeeCutoffs < ActiveRecord::Base
+end
---
+++ test/models/fee_cutoffs_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FeeCutoffsTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
