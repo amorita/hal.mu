b918b982f3e033db6f286aa627bad3a7cc9b95a0
yucjk0120 <yu@hal.mu>
2015-08-29 13:39:24 +0900

    ticket reservation

--- config/routes.rb
+++ config/routes.rb
@@ -108,6 +108,9 @@ end
   resources :account_transactions
   post 'account_transactions/import'
 
+  get 'ticket_reservations/edit' => 'ticket_reservations#edit'
+  post 'ticket_reservations/update' => 'ticket_reservations#update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
