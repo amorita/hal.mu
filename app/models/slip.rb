9d11ce8ff602efba2c22276b75f31a660257a23a
yu <yu@hal.mu>
2016-01-19 19:32:21 +0900

    bug fix

--- app/models/slip.rb
+++ app/models/slip.rb
@@ -1,6 +1,7 @@
 class Slip < ActiveRecord::Base
   has_many :slip_dtls
   has_many :vouchers
+  has_many :slip_mappings
   belongs_to :user
   accepts_nested_attributes_for :slip_dtls, allow_destroy: true
   accepts_nested_attributes_for :vouchers, allow_destroy: true
