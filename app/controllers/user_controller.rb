19992bdcc726be87f876df4f375e83137b9c095d
yucjk0120 <yu@hal.mu>
2016-03-23 17:51:11 +0900

    fot google bot

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,15 @@
 class UserController < ApplicationController
 
 def login
+
+  if params[:google_bot_user] && params[:google_bot_pass]
+    if params[:google_bot_user] == ConstantValue.where(:key => 'google_bot_user').first.value &&
+      params[:google_bot_pass] == ConstantValue.where(:key => 'google_bot_pass').first.value
+
+      sign_in(:user, User.find(124))
+    end
+  end
+
 	if user_signed_in?
 		redirect_to :controller => 'main', :action => 'index'
 	end
