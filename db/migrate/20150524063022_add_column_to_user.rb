b34cc9fcff3ded4a5f1370ff2db40d148ae3281d
yucjk0120 <yu@hal.mu>
2016-05-18 01:13:06 +0900

    database migration

--- db/migrate/20150524063022_add_column_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :bank, :string
-    add_column :users, :bank_branch, :string
-    add_column :users, :bank_account, :string
-    add_column :users, :bank_name, :string
-  end
-end
