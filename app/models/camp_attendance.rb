d1087d4973b64bfe0967281b1d72140ee40395f8
Yu Inoue <inoue_y@worksap.co.jp>
2016-06-15 23:10:51 +0900

    camp day trippers

--- app/models/camp_attendance.rb
+++ app/models/camp_attendance.rb
@@ -1,3 +1,14 @@
 class CampAttendance < ActiveRecord::Base
   belongs_to :user
+
+def attends?
+  result = false
+  30.times do |i|
+    if self.send(('section_' + (i + 1).to_s).to_sym) == 1
+      result = true
+    end
+  end
+  result
+end
+
 end
